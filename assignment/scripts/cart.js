a8b423d7457758336f4691cb74d08779b26dfd0a
Abdulahi Kabare <abdullahikabare@gmail.com>
2023-11-19 16:07:58 -0600

    working progress

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,8 +2,6 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
-let basket = [];
-
 
 // Create a function called `addItem`. It should:
 
@@ -19,13 +17,43 @@ function addItem(item) {
 
 }
 addItem('item');
+addItem('Water');
+addItem('Soda');
+
+console.log('Inside basket:', basket)
+
+
+// Create a function called `listItems`. It should:
+
+// - loop over the items in the `basket` array
+// - console.log each individual item on a new line
+
+// - Create a function called `empty`. It should:
+// - reset the `basket` to an empty array
+
+
 
 
 
 function listItems() {
 
+    for (let item of basket) {
+        console.log('Inside item:', item)
+    }
+    return true;
 }
 
+
+function empty() {
+    for (let i = 0; i <= basket.length + 1; i++) {
+        basket.pop();
+    }
+
+    return true;
+}
+
+empty();
+console.log(basket)
 
 
 
